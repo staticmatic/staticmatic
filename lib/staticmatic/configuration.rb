092f27483f7aefd793c28197e1d350651f2b0764
Gordon McCreight <gordon@mccreight.com>
2010-11-21 18:25:27 -0800

    Make the default preview server host "localhost"

--- lib/staticmatic/configuration.rb
+++ lib/staticmatic/configuration.rb
@@ -10,6 +10,7 @@ module StaticMatic
     
     def initialize
       self.preview_server_port = 3000
+      self.preview_server_host = "localhost"
       self.use_extensions_for_page_links = true
       self.sass_options = {}
       self.haml_options = {}
